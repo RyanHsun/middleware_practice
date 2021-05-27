a73e3476c2332c000406072914ae79f5d5280e20
Ryan Hsun <lenxun.huang@gmail.com>
2021-05-27 17:15:05 +0800

    feat: update app.js(Q2)

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ const app = express()
 const port = 3000
 
 app.use((req, res, next) => {
-  console.log(`${new Date().toLocaleString()} | ${req.method} from ${req.url}`)
+  const processStart = new Date()
+  console.log(`收到請求：${processStart.toLocaleString()} | ${req.method} from ${req.url}`)
+  res.on('finish', () => {
+    const processEnd = new Date()
+    const durationTime = (processEnd - processStart)
+    console.log(`送出回應：${processEnd.toLocaleString()} | ${req.method} from ${req.originalUrl} | total time : ${durationTime}ms`)
+  })
   next()
 })
 
 const auth = (req, res, next) => {
-  if (req.url === '/new') {
+  if (req.url === '/') {
     next()
   } else {
     res.send('此頁面目前不開放！')
@@ -17,15 +23,15 @@ const auth = (req, res, next) => {
 }
 
 app.get('/', auth, (req, res) => {
-  res.send('列出全部 Todo')
-})
-
-app.get('/new', auth, (req, res) => {
-  res.send(`新增 Todo 頁面
+  res.send(`列出全部 Todo
   <form action="/" method="POST">
     <button type="submit">新增</button>
   </form>
   `)
+})
+
+app.get('/new', auth, (req, res) => {
+  res.send('新增 Todo 頁面')
 })
 
 app.get('/:id', (req, res) => {
